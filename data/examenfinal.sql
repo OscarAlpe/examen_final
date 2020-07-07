bd9d3b5a680c12dda6c83e065250ebe9dbd061e5
Oscar Megía López <pfgytk@gmail.com>
2020-07-07 09:52:33 +0200

    ejercicio13

--- data/examenfinal.sql
+++ data/examenfinal.sql
@@ -1,3 +1,7 @@
+--
+-- Inicio
+--
+
 -- 
 -- Set character set the client will use to send SQL statements to the server
 --
@@ -50,4 +54,53 @@ CREATE TABLE IF NOT EXISTS cursos (
 ENGINE = INNODB,
 CHARACTER SET utf8,
 COLLATE utf8_spanish_ci;
+
+--
+-- Create table `cursos`
+--
+CREATE TABLE IF NOT EXISTS cursos (
+  codigoCurso int(11) NOT NULL AUTO_INCREMENT,
+  titulo varchar(255) DEFAULT NULL,
+  descripcion varchar(255) DEFAULT NULL,
+  textoCorto text DEFAULT NULL,
+  fechaComienzo datetime DEFAULT NULL,
+  fechaFin datetime DEFAULT NULL,
+  duracionHoras int(11) DEFAULT NULL,
+  destacado tinyint(1) DEFAULT NULL,
+  foroPortada varchar(255) DEFAULT NULL,
+  pdf varchar(255) DEFAULT NULL,
+  comenzado tinyint(1) DEFAULT NULL,
+  finalizado tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (codigoCurso)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8,
+COLLATE utf8_spanish_ci;
+
+--
+-- Create table `inscripciones`
+--
+CREATE TABLE IF NOT EXISTS inscripciones (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  nombre varchar(255) DEFAULT NULL,
+  apellidos varchar(255) DEFAULT NULL,
+  email varchar(50) DEFAULT NULL,
+  fechaNacimiento datetime DEFAULT NULL,
+  estudios varchar(255) DEFAULT NULL,
+  telefono varchar(255) DEFAULT NULL,
+  Desempleado tinyint(1) DEFAULT NULL,
+  curso int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8,
+COLLATE utf8_spanish_ci;
+
+ALTER TABLE inscripciones ADD FOREIGN KEY FK_inscripciones_cursos_codigoCurso (curso)
+  REFERENCES cursos(codigoCurso)
+  ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Fin
+--
 
